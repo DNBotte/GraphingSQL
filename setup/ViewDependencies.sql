49f01fbddcf3e0ceca1c4a3369f66f27add13376
David Botte <davidnbotte@yahoo.com>
2026-05-01 15:06:18 -0400

    Update ViewDependencies.sql
    fix GitHub reformatting

--- setup/ViewDependencies.sql
+++ setup/ViewDependencies.sql
@@ -1,25 +1,28 @@
 -- EXPORT DEPENDENCIES FROM SQL SERVER
-
-;CREATE OR ALTER VIEW ViewDependencies AS
-
+;
+CREATE OR ALTER VIEW ViewDependencies AS
 SELECT
-    OBJECT_SCHEMA_NAME(o.object_id)  AS source_schema,
+    OBJECT_SCHEMA_NAME(o.object_id) AS source_schema,
     o.name AS source_name,
     o.type_desc AS source_type,
     COALESCE(ref.referenced_schema_name, 'dbo') AS target_schema,
     ref.referenced_entity_name AS target_name,
     COALESCE(t.type_desc, v2.type_desc, 'UNKNOWN') AS target_type
 FROM sys.objects o
-  JOIN sys.sql_expression_dependencies ref
+JOIN sys.sql_expression_dependencies ref
     ON o.object_id = ref.referencing_id
-  LEFT JOIN sys.objects t
+LEFT JOIN sys.objects t
     ON t.name = ref.referenced_entity_name
     AND OBJECT_SCHEMA_NAME(t.object_id) = COALESCE(ref.referenced_schema_name, 'dbo')
-  LEFT JOIN sys.views v2
+LEFT JOIN sys.views v2
     ON v2.name = ref.referenced_entity_name
-WHERE 
-  o.type IN ('V')  -- views only as sources; remove to include procs/functions
-  AND ref.referenced_class = 1
-/*ORDER BY 
+WHERE
+    o.type IN ('V')
+    AND ref.referenced_class = 1
+/*
+
+ORDER BY 
   source_schema, 
-  source_name; */
+  source_name
+*/
+;
